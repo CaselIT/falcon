0cd41cf2a616f04290833b7e811eebc5b3ff29c0
Federico Caselli <cfederico87@gmail.com>
2020-08-06 21:45:02 +0200

    chore(wheels): create action for wheels

--- setup.py
+++ setup.py
@@ -135,7 +135,8 @@ def load_description():
 
 
 setup(
-    name='falcon',
+    # TODO change
+    name='falcon-caselit',
     version=VERSION,
     description='An unladen web framework for building APIs and app backends.',
     long_description=load_description(),
